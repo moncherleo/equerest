3395624c541583c21bdf5949a0cda6f859f26289
aBulgakoff <a.p.bulgakoff@gmail.com>
2016-09-22 00:42:06 +0300

    implemented parametrized test data input

--- src/test/java/com/equerest/tests/webdriver/adminpanel/projectpage/projectsections/EditProjectDescription.java
+++ src/test/java/com/equerest/tests/webdriver/adminpanel/projectpage/projectsections/EditProjectDescription.java
@@ -4,30 +4,47 @@ import com.equerest.pages.adminpanel.ProjectsPage;
 import com.equerest.pages.adminpanel.projectpage.SelectedProjectPage;
 import com.equerest.pages.common.MainPage;
 import com.equerest.tests.webdriver.BaseTest;
+import junitparams.FileParameters;
+import junitparams.JUnitParamsRunner;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Created by cherleo on 9/19/16.
  */
+@RunWith(JUnitParamsRunner.class)
 public class EditProjectDescription extends BaseTest {
 
-    private final static String LOGIN = "olegftzi@gmail.com";
-    private final static String PASSWORD = "Oleg1234";
-    private final static String PROJECT_NAME = "Интернет киоски \"Инетик\"";
-
-    @Test
-    public void EditProjectOnDashboard() {
+    private SelectedProjectPage editProject(String login, String password, String projectName) {
         MainPage mainPage = new MainPage("https://dev.equerest.com/", driver);
         ProjectsPage projectsPage = mainPage.openLoginPage()
-                .loginAs(LOGIN, PASSWORD);
+                .loginAs(login, password);
 
-        SelectedProjectPage selectedProject = projectsPage.navigateToProjectPageViaMenuItem()
-                .editProjectByName(PROJECT_NAME);
+        return projectsPage.navigateToProjectPageViaMenuItem()
+                .editProjectByName(projectName);
+    }
+
+    @Test
+    @FileParameters("src/test/resources/login_project_info.csv")
+    public void editProjectDescriptionPositive(String login, String password, String projectName) {
+        SelectedProjectPage selectedProject = editProject(login, password, projectName);
 
         selectedProject.editProjectDescription()
                 .editText("Новый проект")
                 .applyChanges();
 
     }
+
+    @Test
+    @FileParameters("src/test/resources/login_project_info.csv")
+    public void editProjectDescriptionDeclineChanges(String login, String password, String projectName) {
+        SelectedProjectPage selectedProject = editProject(login, password, projectName);
+
+        selectedProject.editProjectDescription()
+                .editText("Временное описание")
+                .discardChanges();
+    }
+
+
 
 }
