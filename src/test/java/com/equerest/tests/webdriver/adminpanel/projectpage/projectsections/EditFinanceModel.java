ebf457426f3df4d01880e5a854220e33d2762bef
aBulgakoff <a.p.bulgakoff@gmail.com>
2016-09-22 00:23:50 +0300

    refactored template

--- src/test/java/com/equerest/tests/webdriver/adminpanel/projectpage/projectsections/EditFinanceModel.java
+++ src/test/java/com/equerest/tests/webdriver/adminpanel/projectpage/projectsections/EditFinanceModel.java
@@ -1,20 +1,33 @@
 package com.equerest.tests.webdriver.adminpanel.projectpage.projectsections;
 
+import com.equerest.pages.adminpanel.ProjectsPage;
+import com.equerest.pages.adminpanel.projectpage.SelectedProjectPage;
+import com.equerest.pages.common.MainPage;
 import com.equerest.tests.webdriver.BaseTest;
+import org.junit.Ignore;
+import org.junit.Test;
 
 /**
  * Created by aBulgakoff on 9/21/16.
  */
 public class EditFinanceModel extends BaseTest {
 
-//    @Ignore
-//    @Test
-//    public void editFinanceModel() {
-//        selectedProject.editFinanceModel()
-//                .uploadFinancePlan("")
-//                .discardChanges()
-//                .editFinanceModel()
-//                .uploadFinancePlan("")
-//                .applyChanges();
-//    }
+    private final static String LOGIN = "olegftzi@gmail.com";
+    private final static String PASSWORD = "Oleg1234";
+    private final static String PROJECT_NAME = "Интернет киоски \"Инетик\"";
+
+    @Ignore
+    @Test
+    public void EditProjectOnDashboard() {
+        MainPage mainPage = new MainPage("https://dev.equerest.com/", driver);
+        ProjectsPage projectsPage = mainPage.openLoginPage()
+                .loginAs(LOGIN, PASSWORD);
+
+        SelectedProjectPage selectedProject = projectsPage.navigateToProjectPageViaMenuItem()
+                .editProjectByName(PROJECT_NAME);
+
+        selectedProject.editFinanceModel()
+                .uploadFinancePlan("")
+                .applyChanges();
+    }
 }
