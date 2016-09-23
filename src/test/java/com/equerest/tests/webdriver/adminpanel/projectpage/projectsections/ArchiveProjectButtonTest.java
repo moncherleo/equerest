5b138d39c54e8106a74d5a591882b4bc92840e45
EduardAliiev <edeku17@gmail.com>
2016-09-23 20:34:19 +0300

    added: 'InArcive' button @Test

---
+++ src/test/java/com/equerest/tests/webdriver/adminpanel/projectpage/projectsections/ArchiveProjectButtonTest.java
@@ -0,0 +1,44 @@
+package com.equerest.tests.webdriver.adminpanel.projectpage.projectsections;
+
+
+import com.equerest.pages.adminpanel.ProjectsPage;
+import com.equerest.pages.adminpanel.projectpage.InArchivePage;
+import com.equerest.pages.common.MainPage;
+import com.equerest.tests.webdriver.BaseTest;
+import junitparams.FileParameters;
+import junitparams.JUnitParamsRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ * Created by Eduard Aliiev on 9/23/16;
+ */
+
+@RunWith(JUnitParamsRunner.class)
+public class ArchiveProjectButtonTest extends BaseTest {
+    private InArchivePage inArchivePage(String login, String password, String projectName) {
+        MainPage mainPage = new MainPage("https://dev.equerest.com/", driver);
+        ProjectsPage projectsPage = mainPage.openLoginPage()
+                .loginAs(login, password);
+
+        return projectsPage.navigateToProjectPageViaMenuItem()
+                .addedSelectedProject(projectName);
+    }
+
+    //added project-card at "В архив"
+    @Test
+    @FileParameters("src/test/java/com/eduardaliiev/app/equerest/resources/login_project_inArchiveButton_info.csv")
+    public void inArchiveProjectPage(String login, String password, String projectName) {
+        InArchivePage selectedProject = inArchivePage(login, password, projectName);
+
+        Date date = new Date();
+        String currentDate = new Timestamp(date.getTime()).toString();
+
+        selectedProject.addedProjectInArchive()
+                        .verifyProjectSavedAlertDisplayed();
+
+    }
+}
