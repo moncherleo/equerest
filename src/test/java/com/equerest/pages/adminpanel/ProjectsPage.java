0c13c3635d67815fa119503ad30b11936302ff23
EduardAliiev <edeku17@gmail.com>
2016-09-25 01:01:21 +0300

    added: new method in the Project page->Active section->EditInvestmentIncomeSection

--- src/test/java/com/equerest/pages/adminpanel/ProjectsPage.java
+++ src/test/java/com/equerest/pages/adminpanel/ProjectsPage.java
@@ -2,6 +2,7 @@ package com.equerest.pages.adminpanel;
 
 import com.equerest.pages.adminpanel.projectpage.EditProjectPage;
 import com.equerest.pages.adminpanel.projectpage.InArchivePage;
+import com.equerest.pages.adminpanel.projectpage.activesection.EditActiveProjectPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -9,19 +10,30 @@ import org.openqa.selenium.WebDriver;
  * Created by cherleo on 9/19/16.
  */
 public class ProjectsPage extends BaseAdminPanelPage {
-
+    //'Moderation' section "Модерация"
     private By projectFilterModerationButton = By.xpath("//*[@class='projects-list container ng-isolate-scope']//div[1]//*[text()='Модерация']");
     private By projectSearchField = By.xpath("//*[@id='wrapper']/main/div/div/div/div[2]/div/div[2]/div[1]/div[2]/label/../*[@id='searchModerate']");
 
+    //"Редактировать" button on 'Moderation' section
     private By editProjectButton = By.cssSelector(".btn.btn-primary");
 
-    //added project-card at "В архив" button
+    //added project-card at "В архив" button on 'Moderation' section
     private final By projectInArchiveButton = By.xpath("//*[@id='wrapper']/main/div/div/div/div[2]/div/div[2]/div[2]/div/div[2]/div/div[3]//*[text()[contains(.,'В архив')]]");
+
+
+    //'Active' section "Активные"
+    private final By projectFilterActiveButton = By.xpath("//*[@class='projects-list container ng-isolate-scope']//div[1]//*[text()='Активные']");
+    private final By projectActiveSearchField = By.xpath("//*[@id='wrapper']/main/div/div/div/div[2]/div/div[3]/div[1]/div[2]/label/../*[@id='searchActive']");
+
+    //"Редактировать" button on 'Active' section
+    private By editActiveProjectButton = By.cssSelector(".btn.btn-primary");
+
 
     public ProjectsPage(WebDriver driver) {
         super(driver);
     }
 
+    //'Moderation' section "Модерация"
     public ProjectsPage selectModerationFilter(){
         driver.findElement(projectFilterModerationButton).click();
         return this;
@@ -37,6 +49,21 @@ public class ProjectsPage extends BaseAdminPanelPage {
         return new EditProjectPage(driver);
     }
 
+    //'Active' section "Активные"
+    public ProjectsPage selectActiveFilter() {
+        driver.findElement(projectFilterActiveButton).click();
+        return this;
+    }
+    public ProjectsPage searchActiveProjectByName(String projectName){
+        driver.findElement(projectActiveSearchField).sendKeys(projectName);
+        return this;
+    }
+    public EditActiveProjectPage editSelectedActiveProject(){
+        driver.findElement(editActiveProjectButton).click();
+        return new EditActiveProjectPage(driver);
+    }
+
+    //'Moderation' section "Модерация"
     /** Method consolidate actions needed for typical flow of editing a project
      * @param projectName name of project that should be edited*/
     public EditProjectPage editProjectByName(String projectName){
@@ -44,12 +71,21 @@ public class ProjectsPage extends BaseAdminPanelPage {
         searchProjectByName(projectName);
         return editSelectedProject();
     }
-    //added project-card at "В архив" button
+    //added project-card at "В архив" button on 'Moderation' section
     public InArchivePage addedSelectedProject(String projectName){
         selectModerationFilter();
         searchProjectByName(projectName);
         driver.findElement(projectInArchiveButton).click();
         return new InArchivePage(driver);
     }
+
+    //'Active' section "Активные"
+    //in the "Инвестиционный доход" section is edited values.
+    public EditActiveProjectPage editActiveProjectByName(String projectName){
+        selectActiveFilter();
+        searchActiveProjectByName(projectName);
+        return editSelectedActiveProject();
+    }
+
 
 }
--- src/test/java/com/equerest/tests/webdriver/adminpanel/projectpage/activesection/EditActiveProjectPageTest.java
+++ src/test/java/com/equerest/tests/webdriver/adminpanel/projectpage/activesection/EditActiveProjectPageTest.java
@@ -1,7 +1,7 @@
-package com.equerest.tests.webdriver.adminpanel.projectpage.projectsections;
+package com.equerest.tests.webdriver.adminpanel.projectpage.activesection;
 
 import com.equerest.pages.adminpanel.ProjectsPage;
-import com.equerest.pages.adminpanel.projectpage.EditProjectPage;
+import com.equerest.pages.adminpanel.projectpage.activesection.EditActiveProjectPage;
 import com.equerest.pages.common.MainPage;
 import com.equerest.tests.webdriver.BaseTest;
 import junitparams.FileParameters;
@@ -9,64 +9,30 @@ import junitparams.JUnitParamsRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.sql.Timestamp;
-import java.util.Date;
-
 /**
- * Created by cherleo on 9/19/16.
+ * Created by Eduard Aliiev on 9/24/16;
  */
 @RunWith(JUnitParamsRunner.class)
-public class EditProjectDescriptionTest extends BaseTest {
+public class EditActiveProjectPageTest extends BaseTest {
 
-    private EditProjectPage editProject(String login, String password, String projectName) {
+    private EditActiveProjectPage editActiveProject(String login, String password, String projectName) {
         MainPage mainPage = new MainPage("https://dev.equerest.com/", driver);
         ProjectsPage projectsPage = mainPage.openLoginPage()
                 .loginAs(login, password);
 
         return projectsPage.navigateToProjectPageViaMenuItem()
-                .editProjectByName(projectName);
+                .editActiveProjectByName(projectName);
     }
 
+    //"Инвестиционный доход" section is edited values.
     @Test
-    @FileParameters("src/test/resources/login_project_info.csv")
-    public void editProjectDescriptionPositive(String login, String password, String projectName) {
-        EditProjectPage selectedProject = editProject(login, password, projectName);
-
-        Date date= new java.util.Date();
-        String currentDate = new Timestamp(date.getTime()).toString();
-
-        selectedProject.editProjectDescription()
-                .editText(currentDate)
-                .applyChanges()
-                .verifyEditWindowNotPresent()
-                .verifyProjectSavedAlertDisplayed()
-                .assertProjectDescriptionUpdate(currentDate);
-
-    }
-
-    @Test
-    @FileParameters("src/test/resources/login_project_info.csv")
-    public void editProjectDescriptionDeclineChanges(String login, String password, String projectName) {
-        EditProjectPage selectedProject = editProject(login, password, projectName);
-
-        selectedProject.editProjectDescription()
-                .editText("Временное описание")
-                .discardChanges();
-    }
-
-    //upload "Видео" into the project-card
-    @Test
-    @FileParameters("src/test/resources/login_project_info.csv")
+    @FileParameters("src/test/resources/login_project_ActiveSection_info.csv")
     public void uploadVideoInProject(String login, String password, String projectName) {
-        EditProjectPage selectedProject = editProject(login, password, projectName);
+        EditActiveProjectPage selectedProject = editActiveProject(login, password, projectName);
 
-        Date date = new Date();
-        String currentDate = new Timestamp(date.getTime()).toString();
-
-        selectedProject.uploadVideo()
-                //.applyChanges()
-                .verifyEditWindowNotPresent()
-                .verifyProjectSavedAlertDisplayed();
+        selectedProject.investmentIncomeInProject()
+                //.verifyEditWindowNotPresentActiveSection()
+                .verifyProjectSavedAlertDisplayedActiveSection();
     }
 
 
