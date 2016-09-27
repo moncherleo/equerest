8afcd852c6342ee63cfb63626a1d8aa7f3eacd30
Oleg Nesterov <oleg716@gmail.com>
2016-09-28 01:50:02 +0300

    Added tc12 add key numbers, not completed

--- src/test/java/com/equerest/tests/webdriver/adminpanel/projectpage/moderatesection/EditFieldKeyNumbersAddNumbersTest.java
+++ src/test/java/com/equerest/tests/webdriver/adminpanel/projectpage/moderatesection/EditFieldKeyNumbersAddNumbersTest.java
@@ -4,31 +4,23 @@ import com.equerest.pages.adminpanel.ProjectsPage;
 import com.equerest.pages.adminpanel.projectpage.moderatesection.EditProjectPage;
 import com.equerest.pages.common.MainPage;
 import com.equerest.tests.webdriver.BaseTest;
-import junitparams.FileParameters;
-import junitparams.JUnitParamsRunner;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 /**
- * Created by Oleg Nesterov on 27.09.2016;
+ * Created by Oleg Nesterov on 28.09.2016;
  */
-
-@RunWith(JUnitParamsRunner.class)
-public class EditFieldTeamProjectRemovePeopleTest extends BaseTest {
+public class EditFieldKeyNumbersAddNumbersTest extends BaseTest{
     private final static String LOGIN = "olegftzi@gmail.com";
     private final static String PASSWORD = "Oleg1234";
     private final static String PROJECT_NAME = "Интернет киоски \"Инетик\"";
 
-
     @Test
-    public void teamProjectRemovePeople() {
+    public void editKeyNumbersAddNumbers() {
         MainPage mainPage = new MainPage("https://dev.equerest.com/", driver);
         ProjectsPage projectsPage = mainPage.openLoginPage()
                 .loginAs(LOGIN, PASSWORD);
 
         EditProjectPage selectedProject = projectsPage.navigateToProjectPageViaMenuItem()
                 .editProjectByName(PROJECT_NAME);
-
-        selectedProject.teamMemberRemove();
     }
 }
