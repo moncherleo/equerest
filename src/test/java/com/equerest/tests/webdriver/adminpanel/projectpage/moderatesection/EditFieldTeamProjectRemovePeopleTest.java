e60a8605410273bac99cba855aa7d2f4a2de874a
Oleg Nesterov <oleg716@gmail.com>
2016-09-28 01:26:13 +0300

    added tc11 remove people from team

---
+++ src/test/java/com/equerest/tests/webdriver/adminpanel/projectpage/moderatesection/EditFieldTeamProjectRemovePeopleTest.java
@@ -0,0 +1,7 @@
+package com.equerest.tests.webdriver.adminpanel.projectpage.moderatesection;
+
+/**
+ * Created by Oleg Nesterov on 28.09.2016;
+ */
+public class EditFieldTeamProjectRemovePeopleTest {
+}
