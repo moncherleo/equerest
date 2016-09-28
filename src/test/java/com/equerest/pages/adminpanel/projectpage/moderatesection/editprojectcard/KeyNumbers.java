df20135ffe7f3f1e86a4ad072f6a8ecf37d4f11c
Oleg Nesterov <oleg716@gmail.com>
2016-09-28 23:41:11 +0300

    added tc12 add key numbers

---
+++ src/test/java/com/equerest/pages/adminpanel/projectpage/moderatesection/editprojectcard/KeyNumbers.java
@@ -0,0 +1,78 @@
+package com.equerest.pages.adminpanel.projectpage.moderatesection.editprojectcard;
+
+import com.equerest.pages.adminpanel.projectpage.moderatesection.EditProjectPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Oleg Nesterov on 28.09.2016;
+ */
+public class KeyNumbers extends EditProjectPage {
+
+    private By addNumbersElement = By.xpath("//*[@id='wrapper']/main/div/div/div/section[2]/div[2]/div[2]/div/div[1]/h3/span/a");
+    private By editFieldsElement = By.xpath("//*[@id='wrapper']/main/div/div/div/section[2]/div[2]/div[2]/div/div[1]/div/div/div/button");
+    private By fieldNumber = By.id("//*[@id='digit_key_$index']");
+    private By fieldSymbol = By.id("//*[@id='digit_sup_$index']");
+    private By fieldDescription = By.id("//*[@id='digit_descr_$index']");
+    private By applyButton = By.xpath("//*[@id='wrapper']/main/div/div/div/section[2]/div[2]/div[2]/div/div[1]/div/div/div/div[2]/button[2]");
+    private By discardButton = By.xpath("//*[@id='wrapper']/main/div/div/div/section[2]/div[2]/div[2]/div/div[1]/div/div/div/div[2]/button[1]");
+    private By removeButton = By.xpath("//*[@id='wrapper']/main/div/div/div/section[2]/div[2]/div[2]/div/div[1]/div/div/div/div[2]/a");
+
+
+    public KeyNumbers(WebDriver driver){
+        super(driver);
+    }
+
+    @Override
+    public void click(By locator) {
+        super.click(locator);
+    }
+
+    @Override
+    public void fillField(By locator, String data){
+        super.fillField(locator, data);
+    }
+
+
+    public KeyNumbers addNumbersButton(){
+        click(addNumbersElement);
+        return this;
+    }
+    public KeyNumbers editFieldsButton(){
+        click(editFieldsElement);
+        return this;
+    }
+
+    public KeyNumbers fillNumber(String dataNumber){
+        fillField(fieldNumber, dataNumber);
+        return this;
+    }
+
+    public KeyNumbers fillSybmol(String dataSymbol){
+        fillField(fieldSymbol, dataSymbol);
+        return this;
+    }
+
+    public KeyNumbers fillDescription(String dataDescription){
+        fillField(fieldDescription, dataDescription);
+        return this;
+    }
+    public KeyNumbers applyChenges(){
+        click(applyButton);
+        return this;
+    }
+
+    public KeyNumbers DiscardChanges(){
+        click(discardButton);
+        return this;
+    }
+
+    public KeyNumbers removeNumbers(){
+        click(removeButton);
+        return this;
+    }
+
+
+
+
+}
--- src/test/java/com/equerest/tests/webdriver/adminpanel/projectpage/moderatesection/KeyNumbersAddNumbersTest.java
+++ src/test/java/com/equerest/tests/webdriver/adminpanel/projectpage/moderatesection/KeyNumbersAddNumbersTest.java
@@ -2,25 +2,39 @@ package com.equerest.tests.webdriver.adminpanel.projectpage.moderatesection;
 
 import com.equerest.pages.adminpanel.ProjectsPage;
 import com.equerest.pages.adminpanel.projectpage.moderatesection.EditProjectPage;
+import com.equerest.pages.adminpanel.projectpage.moderatesection.editprojectcard.KeyNumbers;
 import com.equerest.pages.common.MainPage;
 import com.equerest.tests.webdriver.BaseTest;
+import junitparams.FileParameters;
+import junitparams.JUnitParamsRunner;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Created by Oleg Nesterov on 28.09.2016;
  */
-public class EditFieldKeyNumbersAddNumbersTest extends BaseTest{
+@RunWith(JUnitParamsRunner.class)
+public class KeyNumbersAddNumbersTest extends BaseTest{
     private final static String LOGIN = "olegftzi@gmail.com";
     private final static String PASSWORD = "Oleg1234";
     private final static String PROJECT_NAME = "Интернет киоски \"Инетик\"";
 
     @Test
-    public void editKeyNumbersAddNumbers() {
+    @FileParameters("src/test/resources/key_numbers_info.csv")
+    public void editKeyNumbersAddNumbers(String number, String symbol, String description) {
         MainPage mainPage = new MainPage("https://dev.equerest.com/", driver);
         ProjectsPage projectsPage = mainPage.openLoginPage()
                 .loginAs(LOGIN, PASSWORD);
 
         EditProjectPage selectedProject = projectsPage.navigateToProjectPageViaMenuItem()
                 .editProjectByName(PROJECT_NAME);
+
+        KeyNumbers keyNumbers = new KeyNumbers(driver);
+        keyNumbers.addNumbersButton()
+                .editFieldsButton()
+                .fillNumber(number)
+                .fillSybmol(symbol)
+                .fillDescription(description)
+                .applyChenges();
     }
 }
