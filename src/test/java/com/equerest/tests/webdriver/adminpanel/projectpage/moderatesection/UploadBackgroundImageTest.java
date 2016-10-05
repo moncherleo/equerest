a20280810eb69e585c4d4ba5f9cec47f1ec3a3f4
EduardAliiev <edeku17@gmail.com>
2016-10-05 21:17:14 +0300

    improvements TC-06: added @Title and @Description

--- src/test/java/com/equerest/tests/webdriver/adminpanel/projectpage/moderatesection/UploadBackgroundImageTest.java
+++ src/test/java/com/equerest/tests/webdriver/adminpanel/projectpage/moderatesection/UploadBackgroundImageTest.java
@@ -8,10 +8,14 @@ import junitparams.JUnitParamsRunner;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.Title;
 
 /**
  * Created by Eduard Aliiev on 10/1/16;
  */
+@Title("TC-06 'Загрузка фонового изображения страницы проекта'")
+@Description("- Пользователь [login] должен предварительно войти в систему, используя [password]")
 @RunWith(JUnitParamsRunner.class)
 public class UploadBackgroundImageTest extends BaseTest {
 
