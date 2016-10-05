a9ea4174631ba808cfe2585c7c12d038deb5659d
Oleg Nesterov <oleg716@gmail.com>
2016-10-05 17:34:31 +0300

    fixed ModerateProjectButtonTest

--- src/test/java/com/equerest/tests/webdriver/adminpanel/projectpage/moderatesection/ModerateProjectButtonTest.java
+++ src/test/java/com/equerest/tests/webdriver/adminpanel/projectpage/moderatesection/ModerateProjectButtonTest.java
@@ -8,20 +8,25 @@ import junitparams.FileParameters;
 import junitparams.JUnitParamsRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.yandex.qatools.allure.annotations.Description;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Oleg Nesterov on 24.09.2016;
  */
 @RunWith(JUnitParamsRunner.class)
 public class ModerateProjectButtonTest extends BaseTest {
-	private ToModeratePage toModeratePage(String login, String password, String projectName) {
+	private ProjectsPage toModeratePage(String login, String password, String projectName) {
 		MainPage mainPage = new MainPage("https://dev.equerest.com/", driver);
 		ProjectsPage projectsPage = mainPage.openLoginPage()
 				.loginAs(login, password);
 
-		return projectsPage.navigateToProjectPageViaMenuItem()
-				.toModerateProject(projectName);
+		return projectsPage.navigateToProjectPageViaMenuItem();
 	}
 
 	//added project-card at "На модерацию"
@@ -29,8 +34,10 @@ public class ModerateProjectButtonTest extends BaseTest {
 	@Description("Изменение статуса проекта на \"Модерация\"")
 	@FileParameters("src/test/resources/login_project_toModerateButton_info.csv")
 	public void toModerateProgectPage(String login, String password, String projectName) {
+
 		ToModeratePage selectedProject = toModeratePage(login, password, projectName)
 				.selectNewFilter()
+				.toModerateProject(projectName)
 				.projectToModerate()
 				.verifyProjectSavedAlertDisplayed();
 	}
