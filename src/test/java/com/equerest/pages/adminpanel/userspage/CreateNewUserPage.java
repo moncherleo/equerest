550d20a56421939f641b21fdf961f4d94cf7a364
Oleg Nesterov <oleg716@gmail.com>
2016-10-07 22:32:13 +0300

    created UsersPage class

---
+++ src/test/java/com/equerest/pages/adminpanel/userspage/CreateNewUserPage.java
@@ -0,0 +1,15 @@
+package com.equerest.pages.adminpanel.userspage;
+
+import com.equerest.pages.adminpanel.UsersPage;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Oleg Nesterov on 07.10.2016;
+ */
+public class CreateNewUserPage extends UsersPage {
+
+    public CreateNewUserPage(WebDriver driver) {
+        super(driver);
+    }
+    //in progress
+}
