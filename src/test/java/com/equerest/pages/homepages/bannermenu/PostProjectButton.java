55bb678e43c627d9747e6ada6532cb0f0a40d45c
EduardAliiev <edeku17@gmail.com>
2016-10-21 09:56:16 +0300

    added TC-129: verefications test for: Post project and How we are working buttons

---
+++ src/test/java/com/equerest/pages/homepages/bannermenu/PostProjectButton.java
@@ -0,0 +1,17 @@
+package com.equerest.pages.homepages.bannermenu;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Eduard Aliiev on 10/20/16;
+ */
+public class PostProjectButton extends BannerMenu {
+
+
+    public PostProjectButton(WebDriver driver) {
+        super(driver);
+    }
+
+
+
+}
