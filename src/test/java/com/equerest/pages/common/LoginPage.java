61ef4de732a73d7a7629eca5414c0c1405867f53
Vladyslav Vlasov <vladyslavvlasov@gmail.com>
2017-01-04 01:46:24 +0200

    Add c13 login test. Update LoginPage with new methods

--- src/test/java/com/equerest/pages/common/LoginPage.java
+++ src/test/java/com/equerest/pages/common/LoginPage.java
@@ -2,8 +2,12 @@ package com.equerest.pages.common;
 
 import com.equerest.pages.AbstractPage;
 import com.equerest.pages.adminpanel.ProjectsPage;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.yandex.qatools.allure.annotations.Step;
 
 /**
@@ -14,6 +18,12 @@ public class LoginPage extends AbstractPage {
     private By emailField = By.id("email");
     private By passwordField = By.id("password");
     private By loginButton = By.cssSelector("#submit-block .button.button-blue");
+    private By loginGButton = By.cssSelector(".button.is-danger");
+    private By loginBlock = By.cssSelector("#login-block");
+    private By loginLabel = By.cssSelector("#login-block>h3");
+    private By emailFieldLabel = By.cssSelector(".input-text-group>label[for = 'email']");
+    private By passwordFieldlabel = By.cssSelector(".input-text-group>label[for = 'password']");
+
 
     public LoginPage(WebDriver driver) {
         super(driver);
@@ -25,5 +35,56 @@ public class LoginPage extends AbstractPage {
         driver.findElement(passwordField).sendKeys(password);
         driver.findElement(loginButton).click();
         return new ProjectsPage(driver);
+    }
+
+    public LoginPage checkThatLoginBlockElementsExist() {
+        WebElement block = driver.findElement(loginBlock);
+        Assert.assertTrue(block.isEnabled());
+        Assert.assertTrue(driver.findElement(emailField).isEnabled());
+        Assert.assertTrue(driver.findElement(passwordField).isEnabled());
+        Assert.assertTrue(driver.findElement(loginButton).isEnabled());
+        Assert.assertTrue(driver.findElement(loginGButton).isEnabled());
+        return new LoginPage(driver);
+    }
+
+    public LoginPage checkThatLoginBlockElementsHaveRightTextValue() {
+        WebElement label = driver.findElement(loginLabel);
+        Assert.assertEquals(label.getText(), "Войти");
+        WebElement emailLabel = driver.findElement(emailFieldLabel);
+        Assert.assertEquals(emailLabel.getText(), "E-mail");
+        WebElement emField = driver.findElement(emailField);
+        Assert.assertEquals(emField.getAttribute("placeholder"), "введите эл. почту");
+        WebElement passLabel = driver.findElement(passwordFieldlabel);
+        Assert.assertEquals(passLabel.getText(), "Пароль");
+        WebElement passField = driver.findElement(passwordField);
+        Assert.assertEquals(passField.getAttribute("placeholder"), "введите пароль");
+        WebElement blueButton = driver.findElement(loginButton);
+        WebElement gButton = driver.findElement(loginGButton);
+        Assert.assertEquals(blueButton.getText(), "Войти");
+        Assert.assertEquals(gButton.getText(), "Войти");
+        return new LoginPage(driver);
+    }
+
+    public LoginPage checkThatLoginBlockElementsClickable() {
+        WebElement blueButton = driver.findElement(loginButton);
+        WebElement gButton = driver.findElement(loginGButton);
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        Assert.assertEquals(blueButton, wait.until(ExpectedConditions.elementToBeClickable(loginButton)));
+        Assert.assertEquals(gButton, wait.until(ExpectedConditions.elementToBeClickable(loginGButton)));
+        return new LoginPage(driver);
+    }
+
+    public LoginPage checkThatTextCanBeEnteredInInputFields() {
+        WebElement emField = driver.findElement(emailField);
+        WebElement passField = driver.findElement(passwordField);
+        String testEmailMsg = "test@mail.ru";
+        String testPwMsg = "Dev12345";
+        emField.clear();
+        emField.sendKeys(testEmailMsg);
+        passField.clear();
+        passField.sendKeys(testPwMsg);
+        Assert.assertEquals(emField.getAttribute("value"), testEmailMsg);
+        Assert.assertEquals(passField.getAttribute("value"), testPwMsg);
+        return new LoginPage(driver);
     }
 }
---
+++ src/test/java/com/equerest/tests/webdriver/common/login/LoginClickableElementsTest.java
@@ -0,0 +1,30 @@
+package com.equerest.tests.webdriver.common.login;
+
+import com.equerest.pages.common.HomePage;
+import com.equerest.pages.common.LoginPage;
+import com.equerest.tests.webdriver.BaseTest;
+import org.junit.Assert;
+import org.junit.Test;
+import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.Title;
+
+/**
+ * Created by Vladyslav Vlasov on 03.01.2017.
+ */
+@Title("C13 Check that block \"Войти\" exists and contains elements")
+@Description("All elements exist and all buttons and links are clickable, all text field can be filled with relevant data")
+public class LoginClickableElementsTest extends BaseTest {
+    @Test
+    public void checkThatLoginBlockExists() {
+        HomePage homePage = new HomePage(driver);
+        homePage
+                .navigateToLoginPage();
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage
+                .checkThatLoginBlockElementsExist()
+                .checkThatLoginBlockElementsHaveRightTextValue()
+                .checkThatLoginBlockElementsClickable()
+                .checkThatTextCanBeEnteredInInputFields();
+    }
+}
